38a9ee5b35f56a37485c0e6855bf61a0edb9060d
Swarle <bogdanvalman@gmail.com>
2024-06-16 19:17:43 +0300

    Added check if match already exists

--- Services/AmourLink.Matching/KafkaHandlers/SwipeMessageHandler.cs
+++ Services/AmourLink.Matching/KafkaHandlers/SwipeMessageHandler.cs
@@ -3,6 +3,7 @@ using AmourLink.InternalCommunication.Kafka.Abstract;
 using AmourLink.InternalCommunication.Kafka.Messages;
 using AmourLink.Matching.Data.Entities;
 using AmourLink.Matching.Helpers;
+using AmourLink.Matching.Specifications;
 
 namespace AmourLink.Matching.KafkaHandlers;
 
@@ -15,6 +16,13 @@ public class SwipeMessageHandler : IMessageHandler<SwipeKafkaMessage>
     }
     public async Task HandleAsync(SwipeKafkaMessage obj, CancellationToken cancellationToken = default)
     {
+        var matchSpecification = new MatchByUsersIdSpecification(obj.FirstUserId, obj.SecondUserId);
+
+        var isMatchAlreadyExist = await _matchRepository.AnyAsync(matchSpecification, cancellationToken);
+
+        if (isMatchAlreadyExist)
+            throw new Exception("Can`t create match because it already exists"); //TODO: Make exception type for exceptions in KafkaHandler
+        
         var match = MatchFactory.CreateMatch(obj.FirstUserId, obj.SecondUserId);
 
         await _matchRepository.CreateAsync(match);
